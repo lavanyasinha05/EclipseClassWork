182f477e8be9c16ec9c9d38a4b39f709af0086c4
Lavanya Sinha <sinhalavanya2403@gmail.com>
2025-07-30 21:30:50 +0530

    cahnged

---
+++ Test/src/HelloTest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class HelloTest {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		// TODO Auto-generated method stub
+		System.out.print("Enter something: ");
+		int a=sc.nextInt();
+		System.out.printf("You entered %d as input",a);
+	}
+
+}
